bcfede5e7e0c6697d87e62a2bffa5a05cde4b399
Cody <cemt5462@gmail.com>
2023-07-12 09:53:08 -0500

    Created setup for API + added seeded data for CRUD

---
+++ MusicLibraryWebAPI/Controllers/SongsController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicLibraryWebAPI.Data;
+using MusicLibraryWebAPI.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MusicLibraryWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SongsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SongsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Songs
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var songs = _context.Songs.ToList();
+            return StatusCode(200, songs);
+        }
+
+        // GET api/<SongsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<Songs
+        [HttpPost]
+        public IActionResult Post([FromBody] Song song)
+        {
+            _context.Songs.Add(song);
+            _context.SaveChanges();
+            return StatusCode(201, song);
+        }
+
+        // PUT api/<SongsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<SongsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ MusicLibraryWebAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,69 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using MusicLibraryWebAPI.Models;
+
+namespace MusicLibraryWebAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Song> Songs { get; set; }
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //Configure the song entitity
+            modelBuilder.Entity<Song>().HasData(
+                new Song
+                {
+                    Id = 1,
+                    Title = "Title",
+                    Artist = "",
+                    Album = "Album",
+                    ReleaseDate = 2000,
+                    Genre = "Rock"
+                },
+                new Song
+                {
+                    Id = 2,
+                    Title = "Title",
+                    Artist = "Artist",
+                    Album = "Album",
+                    ReleaseDate = 2000,
+                    Genre = "Rock"
+                },
+                new Song
+                {
+                    Id = 3,
+                    Title = "Title",
+                    Artist = "Artist",
+                    Album = "Album",
+                    ReleaseDate = 2000,
+                    Genre = "Rock"
+                },
+                new Song
+                {
+                    Id = 2,
+                    Title = "Title",
+                    Artist = "Artist",
+                    Album = "Album",
+                    ReleaseDate = 2000,
+                    Genre = "Rock"
+                },
+                new Song
+                {
+                    Id = 2,
+                    Title = "Title",
+                    Artist = "Artist",
+                    Album = "Album",
+                    ReleaseDate = 2000,
+                    Genre = "Rock"
+                }
+                );
+        }
+
+
+    }
+}
